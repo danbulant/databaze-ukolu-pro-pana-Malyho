975c50f74294da33a7cca81144632e1a94c5e9aa
teolord <74703619+teolord@users.noreply.github.com>
2021-10-31 16:14:49 +0100

    Add files via upload

---
+++ 1.K/přivítání.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PVAukol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int cas = DateTime.Now.Hour;
+
+            Console.Write("Vaše jméno: ");
+            string jmeno = Console.ReadLine();
+            string doba;
+            
+            if (cas < 12)
+            {
+                doba = "rána";
+            }
+
+            else if(cas >= 12 && cas < 18)
+            {
+                doba = "odpoledne";
+            }
+
+
+            else
+            {
+                doba = "večera";
+            }
+
+            Console.WriteLine($"Dobrého {doba}, ti přeji {jmeno}.");
+
+
+
+        }
+    }
+}
